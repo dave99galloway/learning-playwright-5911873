8f0e426c3acc30bf82987efb0debe742446d7b7d
Dave Galloway <dave.00.galloway@gmail.com>
2025-12-05 09:50:23 +0000

    home page tests

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -1,23 +1,37 @@
 import { test, expect } from "@playwright/test";
 
-test("Home", async ({ page }) => {
-  await page.goto("http://practicesoftwaretesting.com");
+test.describe("Home Page", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("http://practicesoftwaretesting.com");
+  });
 
-  await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  test("Has sign in link", async ({ page }) => {
+    await expect(page.getByTestId("nav-sign-in")).toHaveText("Sign in");
+  });
 
-  await expect(page).toHaveTitle("Practice Software Testing - Toolshop - v5.0");
+  test("Has page title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Practice Software Testing - Toolshop - v5.0"
+    );
+  });
 
-  const productGrid = page.locator(".col-md-9");
+  test("Has 9 items loaded by default", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
 
-  await expect(productGrid.getByRole("link")).toHaveCount(9);
-  expect(await productGrid.getByRole("link").count()).toBe(9);
+    await expect(productGrid.getByRole("link")).toHaveCount(9);
+    expect(await productGrid.getByRole("link").count()).toBe(9);
+  });
 
-  await page.getByTestId("search-query").fill("Thor Hammer");
-  await page.getByTestId("search-submit").click();
-  await expect(productGrid.getByRole("link")).toHaveCount(1);
+  test("Search finds Thor's Hammer", async ({ page }) => {
+    const productGrid = page.locator(".col-md-9");
+    
+    await page.getByTestId("search-query").fill("Thor Hammer");
+    await page.getByTestId("search-submit").click();
+    await expect(productGrid.getByRole("link")).toHaveCount(1);
 
-  await expect(productGrid.getByRole("link")).toContainText("Thor Hammer");
-  await expect(
-    productGrid.getByRole("link").nth(0).getByAltText("Thor Hammer")
-  ).toBeVisible();
+    await expect(productGrid.getByRole("link")).toContainText("Thor Hammer");
+    await expect(
+      productGrid.getByRole("link").nth(0).getByAltText("Thor Hammer")
+    ).toBeVisible();
+  });
 });
