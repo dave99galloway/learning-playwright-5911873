c5647a974479b21979821ae78135bd237dc2aa59
Dave Galloway <dave.00.galloway@gmail.com>
2025-12-08 16:51:28 +0000

    api test done

--- tests/api.spec.ts
+++ tests/api.spec.ts
@@ -1,14 +1,32 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, APIResponse } from "@playwright/test";
 import { request } from "http";
-test("GET /products", async ({ request }) => {
+
+test.describe("Products API", () => {
   const apiUrl = "https://api.practicesoftwaretesting.com";
-  const response = await request.get(apiUrl + "/products");
+  var response: APIResponse;
 
-  expect(response.status()).toEqual(200);
-  const body = await response.json();
+  test.beforeEach(async ({ request }) => {
+    response = await request.get(apiUrl + "/products");
+  });
 
-  expect(body.data).toHaveLength(9);
-  expect(body.total).toEqual(5);
+  test("GET /products", async () => {
+    expect(response.status()).toEqual(200);
+    const body = await response.json();
+
+    expect(body.data).toHaveLength(9);
+    expect(body.total).toEqual(50);
+  });
+
+  test("GET /products/{id}", async ({ request }) => {
+    const body = await response.json();
+    const productIdResponse = await request.get(
+      apiUrl + "/products/" + body.data[0].id
+    );
+
+    const prod = await productIdResponse.json()
+
+    expect(prod.brand).toEqual(body.data[0].brand);
+  });
 });
 
 test("POST /users/login", async ({ request }) => {
